43fba07b3871e6a7b7a23babb9749c7b84387a61
Nikolay <enel123@mail.ru>
2019-11-06 23:55:38 +0200

    feat: add animations, add eventListeners, add functions with Ctrl, Alt, Shift and other bottons

--- keys.js
+++ keys.js
@@ -1,113 +1,187 @@
-
-const body = document.querySelector('body')
-const textarea = document.createElement("textarea");
-textarea.autofocus;
-textarea.className = 'textArea';
-const keyboard = document.createElement("div");
-keyboard.className = 'keyboard';
-
-body.appendChild(textarea);
-body.appendChild(keyboard);
-
-function Key(code, isWrite, ru, ruShift, en, enShift ,className) {
-    this.code = code;
-    this.isWrite = isWrite;
-    this.ru = ru;
-    this.ruShift = ruShift;
-    this.en = en;
-    this.enShift = enShift;
-    this.className = className;
-}
-
-
-let keysArray = [
-    new Key('Backquote' , true, 'ё', 'Ё', '`', '~'),
-    new Key('Digit1' , true, 1, '!', 1, '!'),
-    new Key('Digit2' , true, 2, '"', 1, '@'),
-    new Key('Digit3' , true, 3, '№',1, '#'),
-    new Key('Digit4' , true, 4, ';', 1, '$'),
-    new Key('Digit5' , true, 5, '%', 1, '%'),
-    new Key('Digit6' , true, 6, ':', 1, '^'),
-    new Key('Digit7' , true, 7, '?', 1, '&'),
-    new Key('Digit8' , true, 8, '*', 1, '*'),
-    new Key('Digit9' , true, 9, '(', 1, '!'),
-    new Key('Digit0' , true, 0, ')', 1, '!'),
-    new Key('Minus' , true, '-', '_', '-', '_'),
-    new Key('Equal' , true, '=', '+', '=', '+'),
-    new Key('Backspace' , false, 'Backspace', '', '', '' , 'backspace'),
-
-    new Key('Tab' , false, 'Tab', '', '', '', 'tab'),
-    new Key('KeyQ' , true, 'й', 'Й', 'q', 'Q'),
-    new Key('KeyW' , true, 'ц', 'Ц', 'w', 'W'),
-    new Key('KeyE' , true, 'e', 'У', 'e', 'E'),
-    new Key('KeyR' , true, 'к', 'К', 'r', 'R'),
-    new Key('KeyT' , true, 'е', 'Е', 't', 'T'),
-    new Key('KeyY' , true, 'н', 'Н', 'y', 'Y'),
-    new Key('KeyU' , true, 'г', 'Г', 'u', 'U'),
-    new Key('KeyI' , true, 'ш', 'Ш', 'i', 'I'),
-    new Key('KeyO' , true, 'щ', 'Щ', 'o', 'O'),
-    new Key('KeyP' , true, 'з', 'З', 'p', 'P'),
-    new Key('BracketLeft' , true, 'х', 'Х', '[', '{'),
-    new Key('BracketRight' , true, 'ъ', 'Ъ', ']', '}'),
-    new Key('Enter' , false, 'Enter', '', '', '', 'enter'),
-
-    new Key('CapsLock' , false, 'Caps Lock', '', '', '', 'capsLock'),
-    new Key('KeyA' , true, 'ф', 'Ф', 'a', 'A'),
-    new Key('KeyS' , true, 'ы', 'Ы', 's', 'S'),
-    new Key('KeyD' , true, 'в', 'В', 'd', 'D'),
-    new Key('KeyF' , true, 'а', 'А', 'f', 'F'),
-    new Key('KeyG' , true, 'п', 'П', 'g', 'G'),
-    new Key('KeyH' , true, 'р', 'Р', 'h', 'H'),
-    new Key('KeyJ' , true, 'о', 'О', 'j', 'J'),
-    new Key('KeyK' , true, 'л', 'Л', 'k', 'K'),
-    new Key('KeyL' , true, 'д', 'Д', 'l', 'L'),
-    new Key('Semicolon' , true, 'ж', 'Ж', ';', ':'),
-    new Key('Quote' , true, 'э', 'Э', '\'', '"'),
-    new Key('Backslash' , true, '\\', '/', '\\', '|'),
-    new Key('Delete' , false, 'DEL', '', '', '' , 'delete'),
-
-    new Key('ShiftLeft' , false, 'Shift', '', '', '', 'shift'),
-    new Key('KeyZ' , true, 'я', 'Я', 'z', 'Z'),
-    new Key('KeyX' , true, 'ч', 'Ч', 'x', 'X'),
-    new Key('KeyC' , true, 'с', 'С', 'c', 'C'),
-    new Key('KeyV' , true, 'м', 'М', 'v', 'V'),
-    new Key('KeyB' , true, 'и', 'И', 'b', 'B'),
-    new Key('KeyN' , true, 'т', 'Т', 'n', 'N'),
-    new Key('KeyM' , true, 'ь', 'Ь', 'm', 'M'),
-    new Key('Comma' , true, 'б', 'Б', ',', '<'),
-    new Key('Period' , true, 'ю', 'Ю', '.', '>'),
-    new Key('Slash' , true, '.', ',', '/', '?'),
-    new Key('ArrowUp' , false, 'up', '', '', ''),
-    new Key('ShiftRight' , false, 'Shift', '', '', '', 'shift'),
-
-    new Key('ControlLeft' , false, 'Ctrl', '', '', '', 'alt'),
-    new Key('AltLeft' , false, 'Alt', '', '', '', 'alt'),
-    new Key('Space' , true, ' space', 'space ', 'space ', ' space', 'space'),
-    new Key('AltRight' , false, 'Alt', '', '', '', 'alt'),
-    new Key('ArrowLeft' , false, 'left', '', '', ''),
-    new Key('ArrowDown' , false, 'down', '', '', ''),
-    new Key('ArrowRight' , false, 'right', '', '', ''),
-    new Key('ControlRight' , false, 'Ctrl', '', '', '', 'ctrlRight')
-];
-
-createKeyboard('ru', false);
-
-function createKeyboard(lang , shift) {
-    let keyboardContent = '';
-    for(let i = 0; i < keysArray.length; i++){
-        let key = keysArray[i];
-        let className = key.className == undefined?'key':key.className;
-        let keyText = '';
-        if(key.isWrite){
-            keyText = shift == true ? lang == 'ru'? key.ruShift :key.enShift:lang == 'ru'?key.ru:key.en;
-        }
-       else {
-            keyText = key.ru;
-        }
-        keyboardContent += `<div class ="${className}" id = ${key.code}> ${keyText}</div>`
+function choiceEvent(code) {
+    switch (code) {
+        case 'Tab':
+            writeChar('\t');
+            break;
+        case 'CapsLock':
+            capsLookChange();
+            break;
+        case 'ShiftLeft':
+            shiftChange();
+            break;
+        case 'ShiftRight':
+            shiftChange();
+            break;
+        case 'AltRight':
+            alt();
+            break;
+        case 'AltLeft':
+            alt();
+            break;
+        case 'Enter':
+            writeChar('\n');
+            break;
+        case 'Backspace':
+            deleteChar(-1);
+            break;
+        case 'Delete':
+            deleteChar(0);
+            break;
+        case 'ControlLeft':
+            controlChange();
+            break;
+        case 'ControlRight':
+            controlChange();
+            break;
+        case 'ArrowLeft':
+            move('left');
+            break;
+        case 'ArrowUp':
+            move('up');
+            break;
+        case 'ArrowRight':
+            move('right');
+            break;
+        case 'ArrowDown':
+            move('down');
+            break;
+        case 'Space':
+            writeChar(' ');
+            break;
+        default:
+            write(code);
     }
-    keyboard.innerHTML = keyboardContent;
+
 }
 
+function capsLookChange() {
+    caps = !caps;
+    refreshKeyBoard(lang, shift);
+    if(caps){
+        document.querySelector('.capsLock').classList.add('press');
+    }
+    else{
+        document.querySelector('.capsLock').classList.remove('press');
+    }
+}
 
+function shiftChange() {
+    shift = !shift;
+    refreshKeyBoard();
+    if(shift){
+        document.getElementById('ShiftLeft').classList.add('press');
+        document.getElementById('ShiftRight').classList.add('press');
+    }
+    else{
+        document.getElementById('ShiftLeft').classList.remove('press');
+        document.getElementById('ShiftRight').classList.remove('press');
+    }
+}
 
+function writeChar(char) {
+    let start = textarea.selectionStart;
+    let text = textarea.value.split('');
+    text.splice( start, 0, char );
+    let value = '';
+    for(let i = 0; i<text.length; i++){
+        value += text[i];
+    }
+    textarea.value = value;
+    textarea.selectionStart = start + 1;
+    textarea.selectionEnd = textarea.selectionStart;
+}
+
+function alt() {
+    if(shift) {
+        lang = lang == 'ru' ? 'en' : 'ru';
+        refreshKeyBoard();
+    }
+}
+
+function  deleteChar(count) {
+    let start = textarea.selectionStart;
+    let end = textarea.selectionEnd;
+    let text = textarea.value.split('');
+    if(start == end) {
+        text.splice(start == 0? 0: start + count, 1, '');
+    }
+    else{
+        text.splice(start == 0? 0: start + count, end - start, '');
+    }
+    let value = '';
+    for(let i = 0; i<text.length; i++){
+        value += text[i];
+    }
+    textarea.value = value;
+    textarea.selectionStart = start == end? start + count: start ;
+    textarea.selectionEnd = textarea.selectionStart;
+}
+
+function controlChange(){
+    ctrl = !ctrl;
+    if(ctrl){
+        document.getElementById('ControlLeft').classList.add('press');
+        document.getElementById('ControlRight').classList.add('press');
+    }
+    else{
+        document.getElementById('ControlLeft').classList.remove('press');
+        document.getElementById('ControlRight').classList.remove('press');
+    }
+}
+
+function move(side) {
+    let start = textarea.selectionStart;
+    let end = textarea.selectionEnd;
+    if(end == start){
+        if(side == 'left' || side == 'up'){
+            startSelection = 'start';
+        }
+        else{
+            startSelection = 'end';
+        }
+    }
+
+    switch (side) {
+        case 'left':
+            if(ctrl){
+                startSelection == 'end'? end = start : start -= 10 ;
+            }
+            else{
+                startSelection == 'end'? end -- : start -- ;
+            }
+            break;
+        case 'right':
+            if(ctrl){
+                startSelection == 'start'? start = end : end += 10 ;
+            }
+            else{
+                startSelection == 'start'? start ++ : end ++ ;
+            }
+            break;
+        case 'up':
+            startSelection == 'end' ? end = start : start -= 80;
+            break;
+        case 'down':
+            startSelection == 'start' ? start = end : end += 80;
+            break;
+    }
+    if(start < 0){
+        start = 0;
+    }
+    else if(end > textarea.value.length ){
+        end = textarea.value.length ;
+    }
+    if(!shift){
+        startSelection = '';
+        if(side == 'left' || side == 'up'){
+            end = start;
+        }
+        else{
+            start = end;
+        }
+    }
+    console.log('start = ' + start);
+    console.log('end = ' + end);
+    textarea.selectionStart = start;
+    textarea.selectionEnd = end;
+}
